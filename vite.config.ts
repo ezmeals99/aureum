aa737adab15b1b7ac5226c4913f85e774bc194f1
ezmeals99 <ezmeals99@gmail.com>
2026-04-21 18:55:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,13 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/aureum/', 
+    base: '/aureum/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
       alias: {
-        // ඔබගේ components folder එක project එකේ root එකේ ඇති නිසා මෙසේ වෙනස් කරන ලදී
         '@': path.resolve(__dirname, '.'),
       },
     },
